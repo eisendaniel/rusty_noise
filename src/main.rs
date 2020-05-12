296b43b25f7d09a01cc8ba63a5f79346f6045cba
eisendaniel <danieleisen99@gmail.com>
2020-05-12 15:31:23 +1200

    png finished w/ gretscale

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,43 @@
-use rand::Rng;
+use std::env;
 use std::fs::File;
 use std::io::BufWriter;
 use std::path::Path;
 
-fn main() {
-    const WIDTH: u32 = 1920;
-    const HEIGHT: u32 = 1080;
-    const SIZE: u32 = WIDTH * HEIGHT * 4;
+fn gen_pixels(data: &mut Vec<u8>, size: u32) {
+    let mut pixel = 0;
+    loop {
+        if pixel >= size {
+            break;
+        }
+        data.push(rand::random::<u8>());
+        pixel += 1;
+    }
+}
 
-    let mut rng = rand::thread_rng();
+fn main() {
+    //default setiings
+    let is_color = false;
+    let mut width = 128;
+    let mut height = 128;
+    let mut size = width * height;
 
     // For reading and opening files
     let path = Path::new(r"noise.png");
     let file = File::create(path).unwrap();
     let ref mut w = BufWriter::new(file);
-    let mut encoder = png::Encoder::new(w, WIDTH, HEIGHT);
-    encoder.set_color(png::ColorType::RGBA);
-    encoder.set_depth(png::BitDepth::Eight);
-    let mut writer = encoder.write_header().unwrap();
+    let mut encoder = png::Encoder::new(w, width, height);
 
-    let mut data: Vec<u8> = Vec::with_capacity(SIZE as usize);
-
-    let mut sub_pixel = 0;
-    loop {
-        if sub_pixel >= SIZE {
-            break;
-        }
-        data[sub_pixel as usize] = rng.gen();
-        sub_pixel += 1;
+    if is_color {
+        size *= 3;
+        encoder.set_color(png::ColorType::RGB);
+    } else {
+        encoder.set_color(png::ColorType::Grayscale);
     }
+    encoder.set_depth(png::BitDepth::Eight);
+
+    let mut writer = encoder.write_header().unwrap();
+    let mut data: Vec<u8> = Vec::new();
+    gen_pixels(&mut data, size);
+
     writer.write_image_data(&data).unwrap(); // Save
 }
