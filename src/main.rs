40d364c6dc2be1679b4752ed36614973f7eb91e0
eisendaniel <danieleisen99@gmail.com>
2020-05-14 09:41:01 +1200

    fixed color type bug

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,32 @@ use std::fs::File;
 use std::io::BufWriter;
 use std::path::Path;
 
+struct Settings {
+    is_color: bool,
+    width: u32,
+    height: u32,
+    size: u32,
+}
+
+fn parse_args(settings: &mut Settings) {
+    let re = Regex::new(r"\d+x\d+").unwrap();
+    for argument in env::args() {
+        if argument.eq("--color") {
+            settings.is_color = true;
+        } else if argument.eq("--greyscale") {
+            settings.is_color = false;
+        } else if re.is_match(argument.as_str()) {
+            let res: Vec<&str> = argument.split("x").collect();
+            settings.width = res[0].parse().unwrap();
+            settings.height = res[1].parse().unwrap();
+            settings.size = settings.height * settings.width;
+        }
+    }
+    if settings.is_color {
+        settings.size *= 3;
+    }
+}
+
 fn gen_pixels(data: &mut Vec<u8>, size: u32) {
     let mut pixel = 0;
     loop {
@@ -17,41 +43,29 @@ fn gen_pixels(data: &mut Vec<u8>, size: u32) {
 
 fn main() {
     //default setiings
-    let mut is_color = false;
-    let mut width = 128;
-    let mut height = 128;
+    let mut settings = Settings {
+        is_color: false,
+        width: 128,
+        height: 128,
+        size: 128 * 128,
+    };
 
-    let re = Regex::new(r"\d+x\d+").unwrap();
-
-    for argument in env::args() {
-        if argument.eq("--color") {
-            is_color = true;
-        } else if argument.eq("--greyscale") {
-            is_color = false;
-        } else if re.is_match(argument.as_str()) {
-            let res: Vec<&str> = argument.split("x").collect();
-            width = res[0].parse().unwrap();
-            height = res[1].parse().unwrap();
-        }
-    }
-    let mut size = width * height;
+    parse_args(&mut settings);
 
     // For reading and opening files
     let path = Path::new(r"noise.png");
     let file = File::create(path).unwrap();
     let ref mut w = BufWriter::new(file);
-    let mut encoder = png::Encoder::new(w, width, height);
-
-    if is_color {
-        size *= 3;
-        encoder.set_color(png::ColorType::RGB);
-    } else {
-        encoder.set_color(png::ColorType::Grayscale);
-    }
+    let mut encoder = png::Encoder::new(w, settings.width, settings.height);
     encoder.set_depth(png::BitDepth::Eight);
+
+    match settings.is_color {
+        true => encoder.set_color(png::ColorType::RGB),
+        false => encoder.set_color(png::ColorType::Grayscale),
+    }
 
     let mut writer = encoder.write_header().unwrap();
     let mut data: Vec<u8> = Vec::new();
-    gen_pixels(&mut data, size);
+    gen_pixels(&mut data, settings.size);
     writer.write_image_data(&data).unwrap();
 }
